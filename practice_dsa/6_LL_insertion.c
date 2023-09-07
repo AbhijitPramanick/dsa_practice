29f8847e3c0f34446013631e6fa3b9da7d146ab9
AbhijitPramanick <amanik1995@gmail.com>
2023-09-07 21:03:49 +0530

    Insertion at end

---
+++ practice_dsa/6_LL_insertion.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node{
+    int data;
+    struct node * next;
+};
+
+void display(struct node * ptr){
+    printf("\nThe contents of the Linked List:\n");
+    while(ptr!=NULL){
+        printf("\t%d",ptr->data);
+        ptr = ptr->next;
+    }
+}
+void insertEnd(struct node *ptr, int data){
+    //Creating a new node
+    struct node *p;
+    p = (struct node*) malloc(sizeof(struct node));
+    p->data = data;
+    p->next = NULL;
+
+    //Searching for the end node
+    while(ptr->next!=NULL){
+        ptr=ptr->next;
+    }
+    printf("\n\nEnd node having data: %d found. Performing insertion with the new data: %d.\n",ptr->data,p->data);
+
+    //Insertion at the end
+    ptr->next = p;
+
+    printf("Insertion successful.\n");
+}
+int main()
+{
+    struct node *head, *second, *third, *fourth;
+    
+    head = (struct node*) malloc(sizeof(struct node));
+    second = (struct node*) malloc(sizeof(struct node));
+    third = (struct node*) malloc(sizeof(struct node));
+    fourth = (struct node*) malloc(sizeof(struct node));
+
+    head->data = 10;
+    head->next = second;
+    
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = fourth;
+
+    fourth->data = 40;
+    fourth->next = NULL;
+
+    display(head);
+
+    insertEnd(head,100);
+
+    display(head);
+
+    return 0;
+}
